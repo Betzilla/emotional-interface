8b8a980a388c476a08c960ff2eb605b160da42a7
antonleviathan <anton.livaja@gmail.com>
2019-09-09 20:07:12 -0400

    Revert changes

--- sketch.js
+++ sketch.js
@@ -29,14 +29,10 @@
  */
 'use strict';
 let img;
-var blurImg;
-var colors = [];
-var sortMode = null;
 
 function preload() {
   //img = loadImage('heart.png');
   img = createImg('heart.png');
-  createImg('heart.png', setImage);
 }
 
 function setup() {
@@ -84,56 +80,8 @@ function draw() {
   textAlign(LEFT);
   fill(0,0,0);
   text(s, base + 255, mouseY + 80, 330, 70);
-
-  // blur image
-  var tileCount = floor(width / max(mouseX, 5));
-  var rectSize = width / tileCount;
-
-  blurImg.loadPixels();
-  colors = [];
-
-  for (var gridY = 0; gridY < tileCount; gridY++) {
-    for (var gridX = 0; gridX < tileCount; gridX++) {
-      var px = int(gridX * rectSize);
-      var py = int(gridY * rectSize);
-      var i = (py * blurImg.width + px) * 4;
-      var c = color(blurImg.pixels[i], blurImg.pixels[i + 1], blurImg.pixels[i + 2], blurImg.pixels[i + 3]);
-      colors.push(c);
-    }
-  }
-
-  gd.sortColors(colors, sortMode);
-
-  var i = 0;
-  for (var gridY = 0; gridY < tileCount; gridY++) {
-    for (var gridX = 0; gridX < tileCount; gridX++) {
-      fill(colors[i]);
-      rect(gridX * rectSize, gridY * rectSize, rectSize, rectSize);
-      i++;
-    }
-  }
 }
 
 function keyPressed() {
   if (key == 's' || key == 'S') saveCanvas(gd.timestamp(), 'png');
-}
-
-function keyReleased() {
-  if (key == 'c' || key == 'C') writeFile([gd.ase.encode(colors)], gd.timestamp(), 'ase');
-  if (key == 's' || key == 'S') saveCanvas(gd.timestamp(), 'png');
-
-  if (key == '1') loadImage('data/pic1.jpg', setImage);
-  if (key == '2') loadImage('data/pic2.jpg', setImage);
-  if (key == '3') loadImage('data/pic3.jpg', setImage);
-  if (key == '4') loadImage('data/pic4.jpg', setImage);
-
-  if (key == '5') sortMode = null;
-  if (key == '6') sortMode = gd.HUE;
-  if (key == '7') sortMode = gd.SATURATION;
-  if (key == '8') sortMode = gd.BRIGHTNESS;
-  if (key == '9') sortMode = gd.GRAYSCALE;
-}
-
-function setImage(loadedImageFile) {
-  blurImg = loadedImageFile;
 }
