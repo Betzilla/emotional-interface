ee26f4f0c8cabadc76529d84c64dac8e729a5fd0
antonleviathan <anton.livaja@gmail.com>
2019-09-09 20:05:25 -0400

    Test out blur

--- sketch.js
+++ sketch.js
@@ -29,13 +29,18 @@
  */
 'use strict';
 let img;
+var blurImg;
+var colors = [];
+var sortMode = null;
 
 function preload() {
-  img = loadImage('heart.png');
+  //img = loadImage('heart.png');
+  img = createImg('heart.png');
+  createImg('heart.png', setImage);
 }
 
 function setup() {
-  createCanvas(1280, 768);
+  createCanvas(1280, 699);
   // noCursor();
 
   colorMode(HSB, 360, 100, 100);
@@ -46,42 +51,89 @@ function setup() {
 }
 
 function draw() {
-  background(100);
+  let base = 100;
+
+  background(50);
   //insta background
   fill(0,0,100);
-  rect(650, 250, 220, 400);
+  rect(base + 250, 350, 400, 637);
 
   //feed image
   fill(360 - mouseY / 2, 100, 100);
-  rect(650, 225, mouseX/2, mouseX/2);
+  rect(base + 250, 225, mouseX/2, mouseX/2);
 
   //profile name
   textSize(14);
-  text('Betzilla_', 600, 83);
+  text('Betzilla_', base + 130, 83);
 
   //settings icon
   fill(0,0,0);
-  ellipse(745, 75, 3, 3);
-  ellipse(745, 80, 3, 3);
-  ellipse(745, 85, 3, 3);
+  ellipse(base + 405, 75, 3, 3);
+  ellipse(base + 405, 80, 3, 3);
+  ellipse(base + 405, 85, 3, 3);
 
   //profile icon
-  ellipse(570, 80, mouseX/20, mouseX/20);
+  ellipse(base + 90, 80, mouseX/10, mouseX/10);
 
   // like icon
-  image(img, width/2, height/2);
+  image(img, base + 100, 370);
 
   // comments
-  // let time = millis();
-  // rotateX(time / 1000);
-  // rotateZ(time / 1234);
   let s = 'betzilla_happy hushed grin beam tears joy smile upside-down winking star-struck kissing face savoring zany squiting money-mouth hearts halo kiss money-mouth shushing thinking zipper-muth expressionless without mouth hugging tongue smirking unamused rolling eyes grimacing lying relieved pensive';
-  textSize(10);
+  textSize(14);
   textAlign(LEFT);
   fill(0,0,0);
-  text(s, 655, mouseY, 220, 70);
+  text(s, base + 255, mouseY + 80, 330, 70);
+
+  // blur image
+  var tileCount = floor(width / max(mouseX, 5));
+  var rectSize = width / tileCount;
+
+  blurImg.loadPixels();
+  colors = [];
+
+  for (var gridY = 0; gridY < tileCount; gridY++) {
+    for (var gridX = 0; gridX < tileCount; gridX++) {
+      var px = int(gridX * rectSize);
+      var py = int(gridY * rectSize);
+      var i = (py * blurImg.width + px) * 4;
+      var c = color(blurImg.pixels[i], blurImg.pixels[i + 1], blurImg.pixels[i + 2], blurImg.pixels[i + 3]);
+      colors.push(c);
+    }
+  }
+
+  gd.sortColors(colors, sortMode);
+
+  var i = 0;
+  for (var gridY = 0; gridY < tileCount; gridY++) {
+    for (var gridX = 0; gridX < tileCount; gridX++) {
+      fill(colors[i]);
+      rect(gridX * rectSize, gridY * rectSize, rectSize, rectSize);
+      i++;
+    }
+  }
 }
 
 function keyPressed() {
   if (key == 's' || key == 'S') saveCanvas(gd.timestamp(), 'png');
+}
+
+function keyReleased() {
+  if (key == 'c' || key == 'C') writeFile([gd.ase.encode(colors)], gd.timestamp(), 'ase');
+  if (key == 's' || key == 'S') saveCanvas(gd.timestamp(), 'png');
+
+  if (key == '1') loadImage('data/pic1.jpg', setImage);
+  if (key == '2') loadImage('data/pic2.jpg', setImage);
+  if (key == '3') loadImage('data/pic3.jpg', setImage);
+  if (key == '4') loadImage('data/pic4.jpg', setImage);
+
+  if (key == '5') sortMode = null;
+  if (key == '6') sortMode = gd.HUE;
+  if (key == '7') sortMode = gd.SATURATION;
+  if (key == '8') sortMode = gd.BRIGHTNESS;
+  if (key == '9') sortMode = gd.GRAYSCALE;
+}
+
+function setImage(loadedImageFile) {
+  blurImg = loadedImageFile;
 }
